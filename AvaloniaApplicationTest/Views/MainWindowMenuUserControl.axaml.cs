39d8cfb92a037d5ca9800da066321460803c3ebf
m.kagermeier <m.kagermeier@eickhoff-bochum.de>
2024-01-24 14:17:53 +0100

    new panel and exit button works

---
+++ AvaloniaApplicationTest/Views/MainWindowMenuUserControl.axaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplicationTest.Views;
+
+public partial class MainWindowMenuUserControl : UserControl
+{
+    public MainWindowMenuUserControl()
+    {
+        InitializeComponent();
+    }
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void ExitButtonClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        // Schließe das gesamte Programm
+        Environment.Exit(0);
+    }
+}
